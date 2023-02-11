80ed670f9fe77c7a66ea5c8fe117ea00a7c5d007
Archi <pashincev.artur@gmail.com>
2023-02-11 11:00:54 +0300

    Исправлено

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,31 @@
-const buttonAdd = document.querySelector('.profile__edit')
+const buttonEdit = document.querySelector('.profile__edit')
 const buttonDelete = document.querySelector('.popup__close')
-const buttonAppend = document.querySelector('.profile__add')
+const buttonAdd = document.querySelector('.profile__add')
 const popupElement = document.querySelector('.popup')
+const buttonLike = document.querySelectorAll('.card__like');
+const btn = document.querySelector('#btn');
+const userNameValue = document.querySelector('.profile__title');
+const userDescriptionValue = document.querySelector('.profile__subtitle');
+const profilePopupInputName = document.querySelector('.popup__input_user_name');
+const profilePopupInputDescription = document.querySelector('.popup__input_user_description');
+const profilePopupButtonSave = document.querySelector('.popup__button');
 
-buttonAdd.addEventListener('click', function(event) {
-    return popupElement.classList.add('popup_opened')
-    })
-    buttonDelete.addEventListener('click', function (event) {
-    return popupElement.classList.remove('popup_opened')
-    })
+function closePopupButton() {
+    popupElement.classList.remove('popup_opened')
+}
+buttonEdit.addEventListener('click', function(event) {
+    popupElement.classList.add('popup_opened')
+    profilePopupInputName.value = userNameValue.innerHTML;
+    profilePopupInputDescription.value = userDescriptionValue.innerHTML;
+})
 
-    buttonAppend.addEventListener('click', function(event) {
-        return popupElement.classList.add('popup_opened')
-        })
-        buttonDelete.addEventListener('click', function (event) {
-        return popupElement.classList.remove('popup_opened')
-        })
+buttonDelete.addEventListener('click', function (event) {
+    closePopupButton()
+})
 
-
-        const buttonLike = document.querySelectorAll('.card__like')
-        const btn = document.querySelector('#btn');
-        btn.addEventListener('click', () => {
-            return buttonLike.classList.toggle('card__like_active')
-            })
-            
+profilePopupButtonSave.addEventListener('click', function(event) {
+    event.preventDefault();
+    userNameValue.innerHTML = profilePopupInputName.value;
+    userDescriptionValue.innerHTML = profilePopupInputDescription.value;
+    closePopupButton()
+})         
