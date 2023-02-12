4cc8618ebd907f232c9f97d9cf139e7b68bd7c06
agent.archi <pashincev.artur@gmail.com>
2023-02-13 00:30:25 +0300

    corrected

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,28 @@
 const buttonEdit = document.querySelector('.profile__edit')
 const buttonDelete = document.querySelector('.popup__close')
-const buttonAdd = document.querySelector('.profile__add')
 const popupElement = document.querySelector('.popup')
-const buttonLike = document.querySelectorAll('.card__like');
-const btn = document.querySelector('#btn');
 const userNameValue = document.querySelector('.profile__title');
 const userDescriptionValue = document.querySelector('.profile__subtitle');
 const profilePopupInputName = document.querySelector('.popup__input_user_name');
 const profilePopupInputDescription = document.querySelector('.popup__input_user_description');
-const profilePopupButtonSave = document.querySelector('.popup__button');
+const popupForm = document.querySelector('.popup__form');
 
 function closePopupButton() {
     popupElement.classList.remove('popup_opened')
 }
 buttonEdit.addEventListener('click', function(event) {
     popupElement.classList.add('popup_opened')
-    profilePopupInputName.value = userNameValue.innerHTML;
-    profilePopupInputDescription.value = userDescriptionValue.innerHTML;
+    profilePopupInputName.value = userNameValue.textContent;
+    profilePopupInputDescription.value = userDescriptionValue.textContent;
 })
 
 buttonDelete.addEventListener('click', function (event) {
     closePopupButton()
 })
 
-profilePopupButtonSave.addEventListener('click', function(event) {
+popupForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    userNameValue.innerHTML = profilePopupInputName.value;
-    userDescriptionValue.innerHTML = profilePopupInputDescription.value;
+    userNameValue.textContent = profilePopupInputName.value;
+    userDescriptionValue.textContent = profilePopupInputDescription.value;
     closePopupButton()
-})         
+})
